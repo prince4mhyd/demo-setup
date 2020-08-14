8f9ab6e8a00cdb4efb8ed2af09104cafac441509
Ranish Ravindran <33920481+prince4mhyd@users.noreply.github.com>
2020-08-15 01:08:28 +0530

    updated

---
+++ TestSolaraJMPBox_64/TestSolaraJMPBox/TestSolaraJMPBox/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TestSolaraJMPBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello User");
+            Console.WriteLine(String.Format("Your Current Directory is : {0}",Environment.CurrentDirectory));
+            Console.ReadLine();
+        }
+    }
+}
